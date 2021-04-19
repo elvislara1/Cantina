043df8a50368e910be1d9522ded455805379c9e6
elvislara1 <elarac01@elpuig.xeill.net>
2021-04-19 19:26:04 +0200

    Prueba

--- app/src/main/java/com/example/cantina/model/Producto.java
+++ app/src/main/java/com/example/cantina/model/Producto.java
@@ -9,14 +9,22 @@ public class Producto {
     public int productoId;
 
     public String nombre;
+    public double preciod;
     public String precio;
-    public String idDrawable;
+    public String img;
     public String tipo;
 
-    public Producto(String nombre, String precio, String idDrawable, String tipo) {
+    public Producto(String nombre, double preciod, String img, String tipo) {
+        this.nombre = nombre;
+        this.preciod = preciod;
+        this.img = img;
+        this.tipo = tipo;
+    }
+
+    public Producto(String nombre, String precio, String img, String tipo) {
         this.nombre = nombre;
         this.precio = precio;
-        this.idDrawable = idDrawable;
+        this.img = img;
         this.tipo = tipo;
     }
 }
